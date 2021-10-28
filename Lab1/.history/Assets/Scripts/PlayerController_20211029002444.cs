0133b701d72806171338a7a3728344937c77eca1
Tania Kobal <=>
2021-10-29 00:47:19 +0300

    created character

---
+++ Lab1/.history/Assets/Scripts/PlayerController_20211029002444.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    //public Rigidbody theRB;
+    public float jumForce;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        // if (Input.GetButtonDown("Jump"))
+        // {
+        //     theRB.velocity = new Vector3(theRB.velocity.x, jumForce, theRB.velocity.z);
+        // }
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        if (controller.)
+        {
+
+        }
+        if (Input.GetButtonDown("Jump")){
+            moveDirection.y = jumForce;
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
